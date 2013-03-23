mpc-hc/mpc-hc

Subtitles: Improve error checking with ASS rendering.
Some crashes could occur when the SSA script was using obviously wrong values.

Uses the same check as libass for maximum glyph size.

src/Subtitles/Rasterizer.cpp
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2012 see Authors.txt
+ * (C) 2006-2013 see Authors.txt
  *
  * This file is part of MPC-HC.
  *
@@ -438,6 +438,12 @@ bool Rasterizer::ScanConvert()
     mWidth  = maxx + 1 - minx;
     mHeight = maxy + 1 - miny;
 
+    // Check that the size isn't completely crazy
+    if (mWidth * mHeight > 8000000) {
+        TRACE(_T("Error in Rasterizer::ScanConvert: size (%dx%d) is too big"), mHeight, mWidth);
+        return false;
+    }
+
     mPathOffsetX = minx;
     mPathOffsetY = miny;
 
@@ -446,10 +452,18 @@ bool Rasterizer::ScanConvert()
     mEdgeNext = 1;
     mEdgeHeapSize = 2048;
     mpEdgeBuffer = (Edge*)malloc(sizeof(Edge) * mEdgeHeapSize);
+    if (!mpEdgeBuffer) {
+        TRACE(_T("Error in Rasterizer::ScanConvert: mpEdgeBuffer is NULL"));
+        return false;
+    }
 
     // Initialize scanline list.
 
     mpScanBuffer = DEBUG_NEW unsigned int[mHeight];
+    if (!mpScanBuffer) {
+        TRACE(_T("Error in Rasterizer::ScanConvert: mpScanBuffer is NULL"));
+        return false;
+    }
     memset(mpScanBuffer, 0, mHeight * sizeof(unsigned int));
 
     // Scan convert the outline.  Yuck, Bezier curves....

jeeb/xy-vsfilter

Backport of MPC-HC WIP commit 7d31a49 (U78's repository)

src/subtitles/Rasterizer.cpp
@@ -2911,12 +2911,28 @@ bool ScanLineData::ScanConvert(const PathData& path_data, const CSize& size)
     }
     mWidth = size.cx;
     mHeight = size.cy;
+
+    // Check that the size isn't completely crazy
+    if (mWidth * mHeight > 8000000) {
+        TRACE(_T("Error in ScanLineData::ScanConvert: size (%dx%d) is too big"), mHeight, mWidth);
+        return false;
+    }
+
     // Initialize edge buffer.  We use edge 0 as a sentinel.
     mEdgeNext = 1;
     mEdgeHeapSize = 2048;
-    mpEdgeBuffer = (Edge*)malloc(sizeof(Edge)*mEdgeHeapSize);
+    mpEdgeBuffer = (Edge*)malloc(sizeof(Edge) * mEdgeHeapSize);
+    if (!mpEdgeBuffer) {
+        TRACE(_T("Error in ScanLineData::ScanConvert: mpEdgeBuffer is NULL"));
+        return false;
+    }
+
     // Initialize scanline list.
     mpScanBuffer = new unsigned int[mHeight];
+    if (!mpScanBuffer) {
+        TRACE(_T("Error in ScanLineData::ScanConvert: mpScanBuffer is NULL"));
+        return false;
+    }
     memset(mpScanBuffer, 0, mHeight*sizeof(unsigned int));
     // Scan convert the outline.  Yuck, Bezier curves....
     // Unfortunately, Windows 95/98 GDI has a bad habit of giving us text