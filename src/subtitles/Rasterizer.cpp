mpc-hc/mpc-hc

Subtitles: Improve error checking with ASS rendering.
Some crashes could occur when the SSA script was using obviously wrong values.

Uses the same check as libass for maximum glyph size.

src/Subtitles/Rasterizer.cpp
@@ -1,6 +1,6 @@
 /*
  * (C) 2003-2006 Gabest
- * (C) 2006-2012 see Authors.txt
+ * (C) 2006-2013 see Authors.txt
  *
  * This file is part of MPC-HC.
  *
@@ -438,6 +438,12 @@ bool Rasterizer::ScanConvert()
     mWidth  = maxx + 1 - minx;
     mHeight = maxy + 1 - miny;
 
+    // Check that the size isn't completely crazy
+    if (mWidth * mHeight > 8000000) {
+        TRACE(_T("Error in Rasterizer::ScanConvert: size (%dx%d) is too big"), mHeight, mWidth);
+        return false;
+    }
+
     mPathOffsetX = minx;
     mPathOffsetY = miny;
 
@@ -446,10 +452,18 @@ bool Rasterizer::ScanConvert()
     mEdgeNext = 1;
     mEdgeHeapSize = 2048;
     mpEdgeBuffer = (Edge*)malloc(sizeof(Edge) * mEdgeHeapSize);
+    if (!mpEdgeBuffer) {
+        TRACE(_T("Error in Rasterizer::ScanConvert: mpEdgeBuffer is NULL"));
+        return false;
+    }
 
     // Initialize scanline list.
 
     mpScanBuffer = DEBUG_NEW unsigned int[mHeight];
+    if (!mpScanBuffer) {
+        TRACE(_T("Error in Rasterizer::ScanConvert: mpScanBuffer is NULL"));
+        return false;
+    }
     memset(mpScanBuffer, 0, mHeight * sizeof(unsigned int));
 
     // Scan convert the outline.  Yuck, Bezier curves....

jeeb/xy-vsfilter

Is edited to use an amount of samples that will either let one use
an effective maximum texture size of 8000x500 or 4000x1000.
This is more strict than the width >4000 OR height >4000 check
that was first decided on, but will let users create longer/higher
pieces of text that get moved on the screen in order to create an
illusion of scrolling.
Backport of MPC-HC commit 2c597d9

src/subtitles/Rasterizer.cpp
@@ -41,6 +41,10 @@
 #define _IMPL_MIN _MIN
 #endif
 
+/* Define for the sanity checks */
+#define BIG_AMOUNT_OF_SAMPLES 256000000
+// Equals (8000 * 8) * (500 * 8)
+
 typedef const UINT8 CUINT8, *PCUINT8;
 
 //NOTE: signed or unsigned affects the result seriously
@@ -2911,12 +2915,30 @@ bool ScanLineData::ScanConvert(const PathData& path_data, const CSize& size)
     }
     mWidth = size.cx;
     mHeight = size.cy;
+
+    // Check that the size isn't completely crazy
+    // FIXME: Reminder that the rendering should be sane'ified one day to not
+    //        take a fuckload of memory with large things.
+    if (mWidth * mHeight > BIG_AMOUNT_OF_SAMPLES) {
+        TRACE(_T("Error in ScanLineData::ScanConvert: size (%dx%d) is too big"), mHeight, mWidth);
+        return false;
+    }
+
     // Initialize edge buffer.  We use edge 0 as a sentinel.
     mEdgeNext = 1;
     mEdgeHeapSize = 2048;
-    mpEdgeBuffer = (Edge*)malloc(sizeof(Edge)*mEdgeHeapSize);
+    mpEdgeBuffer = (Edge*)malloc(sizeof(Edge) * mEdgeHeapSize);
+    if (!mpEdgeBuffer) {
+        TRACE(_T("Error in ScanLineData::ScanConvert: mpEdgeBuffer is NULL"));
+        return false;
+    }
+
     // Initialize scanline list.
     mpScanBuffer = new unsigned int[mHeight];
+    if (!mpScanBuffer) {
+        TRACE(_T("Error in ScanLineData::ScanConvert: mpScanBuffer is NULL"));
+        return false;
+    }
     memset(mpScanBuffer, 0, mHeight*sizeof(unsigned int));
     // Scan convert the outline.  Yuck, Bezier curves....
     // Unfortunately, Windows 95/98 GDI has a bad habit of giving us text