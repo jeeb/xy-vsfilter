594e3de3d16fe919f6fa899afeacd38c636df76d
Alex Smith <theryuu@warpsharp.info>
2015-04-26 04:17:15 -0400

    subtitles: Replace custom malloc with aligned_offset_malloc
    Replaces the custom memalign hack malloc with the msvc implementation.

--- src/subtitles/xy_malloc.cpp
+++ src/subtitles/xy_malloc.cpp
@@ -7,61 +7,31 @@
 #include <malloc.h>
 #include <string.h>
 
-//gathered from x264
-/****************************************************************************
- * xy_malloc:
- ****************************************************************************/
-void* xy_malloc(int i_size, int align_shift)
+#define ALIGN 16
+
+void* xy_malloc(size_t i_size, size_t align_shift)
 {
-    const int ALIGNMENT = 16;
-    const int MASK = ALIGNMENT - 1;
-    uint8_t* buf;
-    uint8_t* align_buf;
-    align_shift &= MASK;
-    buf = (uint8_t*) malloc(i_size + MASK + sizeof(void**) +
-                            sizeof(int));
-    if (!buf) {
+    void* ptr = NULL;
+    ptr = _aligned_offset_malloc(i_size, ALIGN, align_shift);
+    if (!ptr) {
         return nullptr;
     }
-
-    align_buf = buf + MASK + sizeof(void**) + sizeof(int);
-    align_buf -= (ALIGNMENT + ((intptr_t) align_buf & MASK) - align_shift) & MASK;
-    *((void**)(align_buf - sizeof(void**))) = buf;
-    *((int*)(align_buf - sizeof(void**) - sizeof(int))) = i_size;
-    return align_buf;
+    return ptr;
 }
 
-/****************************************************************************
- * xy_free:
- ****************************************************************************/
 void xy_free(void* p)
 {
     if (p) {
-        free(*(((void**) p) - 1));
+        _aligned_free(p);
     }
 }
 
-/****************************************************************************
- * xy_realloc:
- ****************************************************************************/
-void* xy_realloc(void* p, int i_size, int align_shift)
+void* xy_realloc(void* p, size_t i_size, size_t align_shift)
 {
-    int       i_old_size = 0;
-    uint8_t* p_new;
-    if (p) {
-        i_old_size = *((int*)((uint8_t*) p - sizeof(void**) -
-                              sizeof(int)));
-    }
-    p_new = (uint8_t*)xy_malloc(i_size, align_shift);
-    if (!p_new) {
+    void* ptr = NULL;
+    ptr = _aligned_offset_realloc(p, i_size, ALIGN, align_shift);
+    if (!ptr) {
         return nullptr;
     }
-
-    if (i_old_size > 0 && i_size > 0) {
-        memcpy(p_new, p, (i_old_size < i_size) ? i_old_size : i_size);
-    }
-    xy_free(p);
-    return p_new;
+    return ptr;
 }
-
-
